c85c3ee723a072fcc6dc7900149da01510919657
csigildea <gildeaco@tcd.ie>
2018-11-06 22:08:29 +0000

    credentials.py fix

---
+++ scraper/credentials.py
@@ -0,0 +1,12 @@
+# Enter EITHER
+#   - Username and Password
+#   OR
+#   - Personal Access Token 
+# For Github
+
+github_login = {
+    'username' : 'YOUR_USERNAME',
+    'password' : 'YOUR_PASSWORD',
+    'personal_access_token' : 'YOUR_PERSONAL_ACCESS_TOKEN'
+}
+
