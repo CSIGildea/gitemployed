f02606195da823fb6dbd25489b6f38bce61dd79c
csigildea <gildeaco@tcd.ie>
2018-11-06 22:11:12 +0000

    Added comment about token

--- scraper/test_scraper.py
+++ scraper/test_scraper.py
@@ -4,7 +4,7 @@ def test_auth_github_account_invalid_user_and_pass():
     github_ac = scraper.auth_github_account("hackerboi3000","123password","")
     assert github_ac == None
 
-def test_auth_github_account_invalid_token():
+def test_auth_github_account_invalid_token():     # Not real token
     github_ac = scraper.auth_github_account("","","3HnjKB1TnQSiYG3uaTMWD33UF2ZxoSPTP5")
     assert github_ac == None
 
