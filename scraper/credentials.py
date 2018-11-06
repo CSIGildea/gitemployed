b82cdb9433ce21d7d49794ace641dbf9255cd9a6
csigildea <gildeaco@tcd.ie>
2018-11-06 21:24:21 +0000

    Test gitignore

--- scraper/credentials.py
+++ scraper/credentials.py
@@ -7,6 +7,6 @@
 github_login = {
     'username' : 'YOUR_USERNAME',
     'password' : 'YOUR_PASSWORD',
-    'personal_access_token' : 'YOUR_PERSONAL_ACCESS_TOKEN'
+    'personal_access_token' : 'TEST'
 }
 
